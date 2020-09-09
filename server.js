fffa50b82a10d9a53be9453b0eaaf50a29629a97
Luiz Silva <luizsilva.ee@gmail.com>
2020-09-09 15:53:38 -0300

    Arquivos iniciais

---
+++ server.js
@@ -0,0 +1,32 @@
+﻿const express = require("express");
+const nunjucks = require("nunjucks");
+const extenso = require("extenso");
+const path = require("path");
+
+const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true,
+});
+
+app.set("view engine", ".njk");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  return res.render("index");
+});
+
+app.get("/result", (req, res) => {
+  const { value } = req.query;
+
+  //tira os espaços da string
+  const amount = value.replace(/\s+/g, "");
+  console.log(amount);
+  const currencyString = extenso(amount, { mode: "currency" });
+
+  return res.render("result", { currencyString });
+});
+
+app.listen(3000);
